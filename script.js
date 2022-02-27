303d4c16a90a15afc03e5073962812aa80cbae0b
jackere1 <91013518+jackere1@users.noreply.github.com>
2022-02-27 14:49:45 +0800

    second commitment

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.round(Math.random() * 9);
+}
 
+const compareGuesses = (user, computer, target) => {
+    let diffHuman = Math.abs(target-user);
+    let diffComputer = Math.abs(target-computer);
+    if(diffHuman <= diffComputer) return true;
+    return false;
+}
+
+let updateScore = arg => {
+    if(arg == 'human')
+        humanScore++;
+    else computerScore++; 
+}
+
+let advanceRound = () => {
+    currentRoundNumber++;
+} 
